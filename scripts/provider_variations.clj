8e4067f73a04fc5af351c955bea0daf9b8a5d492
chapmanb <chapmanb@50mail.com>
2010-11-06 18:35:36 -0400

    Filter provider variations by those of interest

--- scripts/provider_variations.clj
+++ scripts/provider_variations.clj
@@ -15,18 +15,19 @@
   (:use [clojure.java.io])
   (:require [clojure.contrib.str-utils2 :as str2]))
 
-(defn vrns-from-file [rdr has-header]
+(defn vrns-from-file [rdr has-header vrns-to-get]
   "Retrieve all variations from the provider download file."
   (let [not-comment #(not (.startsWith % "#"))
         noc-lines (filter not-comment (line-seq rdr))
         work-lines (if (= has-header "yes") (rest noc-lines) noc-lines)]
-    (for [line work-lines]
-      (-> line
-        (#(first (str2/split % #"\t")))
-        (#(first (str2/split % #",")))
-        (#(first (str2/split % #"\(")))))))
+    (filter #(contains? vrns-to-get %)
+      (for [line work-lines]
+        (-> line
+          (#(first (str2/split % #"\t")))
+          (#(first (str2/split % #",")))
+          (#(first (str2/split % #"\("))))))))
 
-(defn providers-by-vrn [p-info]
+(defn providers-by-vrn [p-info vrns-to-get]
   "Organize map with variations as keys and providers as values."
   (loop [p-info p-info, vrn-map {}]
     (if (empty? p-info)
@@ -35,7 +36,7 @@
         (with-open [prdr (reader fname)]
           (let [cur-map (reduce (fn [vmap vrn]
                                   (assoc vmap vrn (cons provider (get vmap vrn))))
-                                vrn-map (vrns-from-file prdr has-hdr))]
+                                vrn-map (vrns-from-file prdr has-hdr vrns-to-get))]
             (recur (rest p-info) cur-map)))))))
 
 (defn parse-providers [rdr in-file]
@@ -45,11 +46,19 @@
       (let [[p f h] (str2/split line #",")]
         [p (format "%s/%s" dir f) h]))))
 
+(defn vrns-of-interest [in-file]
+  (with-open [rdr (reader in-file)]
+    (set (distinct
+      (for [line (line-seq rdr)]
+        (first (str2/split line #",")))))))
+
 (when *command-line-args*
   (let [provider-file (first *command-line-args*)
-        out-dir (second *command-line-args*)]
+        data-dir (second *command-line-args*)
+        vrns-to-get (vrns-of-interest (file data-dir "variation-phenotypes.csv"))]
     (with-open [rdr (reader provider-file)
-                w (writer (file out-dir "variation-providers.csv"))]
+                w (writer (file data-dir "variation-providers.csv"))]
       (.write w "variation,providers\n")
-      (doseq [[vrn providers] (providers-by-vrn (parse-providers rdr provider-file))]
-        (.write w (format "%s,%s\n" vrn (str2/join ";" providers)))))))
+      (let [p-info (parse-providers rdr provider-file)]
+        (doseq [[vrn providers] (providers-by-vrn p-info vrns-to-get)]
+          (.write w (format "%s,%s\n" vrn (str2/join ";" providers))))))))
