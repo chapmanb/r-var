505d3dc2821a37666647559cdd06b8b1ff430273
chapmanb <chapmanb@50mail.com>
2010-12-12 08:50:38 -0500

    Utilize app.yaml for GAE configuration

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :keep-non-project-classes true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.4.1"]
-                 [hiccup "0.2.6"]
+                 [compojure "0.5.3"]
+                 [hiccup "0.3.1"]
                  [gaka "0.2.0"]
-                 [ring/ring-core "0.2.5"]
-                 [ring/ring-jetty-adapter "0.2.5"]]
+                 [ring/ring-core "0.3.5"]
+                 [ring/ring-jetty-adapter "0.3.5"]]
   :dev-dependencies [[appengine-magic "0.4.0-SNAPSHOT"]
-                     [vimclojure/server "2.2.0"]
+                     [vimclojure/server "2.3.0-SNAPSHOT"]
                      [mysql/mysql-connector-java "5.1.6"]
                      [clojure-csv/clojure-csv "1.2.0"]]
   :jvm-opts ["-Xmx1512M"])
