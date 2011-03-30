a93197a9ee5ebdcf045f8272c64c330e1063a116
chapmanb <chapmanb@50mail.com>
2011-03-30 08:44:01 -0400

    Modify to new use BioMart API

--- src/rvar/hla.clj
+++ src/rvar/hla.clj
@@ -2,17 +2,19 @@
 (ns rvar.hla
   (:use [clojure.contrib.duck-streams :only [reader]]
         [net.cgrand.enlive-html :only [html-resource select text]])
-  (:require [biomart-client.core :as biomart]))
+  (:require [biomart-client.query :as biomart]
+            [biomart-client.utils :as biomart-utils]))
 
 (defn ensembl-from-hgnc [hgnc]
   "Retrieve Ensembl gene ID from HGNC name using BioMart."
   (let [mart-url "http://www.ensembl.org/biomart"
         ds-name "hsapiens_gene_ensembl"]
-    (-> (biomart/martservice mart-url)
-        (biomart/dataset ds-name)
-        (biomart/query :filter {:chromosome_name "6" :hgnc_symbol hgnc}
-                       :attrs ["ensembl_gene_id" "hgnc_symbol"])
-        ffirst)))
+    (-> (biomart-utils/martservice-url mart-url)
+        (biomart/query {:dataset ds-name
+                        :filter {:chromosome_name "6" :hgnc_symbol hgnc}
+                        :attrs ["ensembl_gene_id" "hgnc_symbol"]})
+        first
+        (get "ensembl_gene_id"))))
 
 (defn hla-names []
   "Retrieve list of HLA names from online repository by screen scraping."
